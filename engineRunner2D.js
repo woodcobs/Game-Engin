d54a4d252341cd5aea11773e656e73c0d3c9fcee
Bradley Woodcock <woodcobs@dukes.jmu.edu>
2022-07-26 10:01:55 -0400

    Added to index.css

--- engineRunner2D.js
+++ engineRunner2D.js
@@ -1,8 +1,8 @@
 // uploading this currently for reference
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-var height = 400;
-var width = 400;
+var height = 880;
+var width = 560;
 var stiffness = 0.5;
 var b = -1;
 var angularB = -7;
@@ -61,26 +61,32 @@ Vector2D.prototype = {
     }
 }
 
+var Circle = function() {
+
+}
+
 // Currently using this for reference
-var Rect = function (x, y, w, h, m) {
-    if (typeof (m) === 'undefined') {
-        this.m = 1;
-    }
+// x and y represent the starting coordinates of the top left of this object.
+// width represents the width of the object
+// height represents the height of the rectangle
+// m represents the mass of the object
+var Rect = function (x, y, w, h, mass = 1) { 
 
     this.width = w;
     this.height = h;
+    this.mass = mass;
 
     this.topLeft = new Vector2D(x, y);
     this.topRight = new Vector2D(x + w, y);
     this.bottomRight = new Vector2D(x + w, y + h);
     this.bottomLeft = new Vector2D(x, y + h);
 
-    this.v = new Vector2D(0, 0);
-    this.a = new Vector2D(0, 0);
-    this.theta = 0;
+    this.velocity = new Vector2D(0, 0); // initial velocity
+    this.acceleration = new Vector2D(0, 0); //initial acceleration
+    this.theta = 0; // angle
     this.omega = 0;
     this.alpha = 0;
-    this.J = this.m * (this.height * this.height + this.width * this.width) / 12000;
+    this.J = this.mass * (this.height * this.height + this.width * this.width) / 12000;
 };
 
 /* The Rect class only defines the four corners of the rectangle, but sometimes we need the center point so we can calulate that by finding the diagonal and cutting it in half. */
@@ -113,66 +119,61 @@ Rect.prototype.move = function (v) {
     return this;
 }
 
-var rect = new Rect(200, 0, 100, 50);
-rect.v = new Vector2D(0, 2);
-var spring = new Vector2D(200, 0);
+height = 880,
+    width = 560,
+    x = 200,
+    y = 0,
+    vy = 0,
+    ay = 0,
+    m = 0.1,    // Ball mass in kg
+    r = 10,     // Ball radius in cm, or pixels.
+    dt = 0.02,  // Time step.
+    e = -0.5,   // Coefficient of restitution ("bounciness")
+    rho = 1.2,  // Density of air. Try 1000 for water.
+    C_d = 0.2, // Coeffecient of drag for a ball
+    A = Math.PI * r * r / 10000 // Frontal area of the ball; divided by 10000 to compensate for the 1px = 1cm relation
+    ;
 
-ctx.strokeStyle = 'black';
+ctx.fillStyle = 'red';
 
-var loop = function () {
-    var f = new Vector2D(0, 0);
-    var torque = 0;
-
-    /* Start Velocity Verlet by performing the translation */
-    var dr = rect.v.scale(dt).add(rect.a.scale(0.5 * dt * dt));
-    rect.move(dr.scale(100));
-
-    /* Add Gravity */
-    f = f.add(new Vector2D(0, rect.m * 9.81));
-
-    /* Add damping */
-    f = f.add(rect.v.scale(b));
-
-    /* Add Spring; we calculate this separately so we can calculate a torque. */
-    var springForce = rect.topLeft.subtract(spring).scale(-1 * stiffness);
-    /* This vector is the distance from the end of the spring to the box's center point */
-    var r = rect.center().subtract(rect.topLeft);
-    /* The cross product informs us of the box's tendency to rotate. */
-    var rxf = r.cross(springForce);
-
-    torque += -1 * rxf;
-    f = f.add(springForce);
-
-    /* Finish Velocity Verlet */
-    var new_a = f.scale(rect.m);
-    var dv = rect.a.add(new_a).scale(0.5 * dt);
-    rect.v = rect.v.add(dv);
-
-    /* Do rotation; let's just use Euler for contrast */
-    torque += rect.omega * angularB; // Angular damping
-    rect.alpha = torque / rect.J;
-    rect.omega += rect.alpha * dt;
-    var deltaTheta = rect.omega * dt;
-    rect.rotate(deltaTheta);
-
+function loop()
+{ 
+    var fy = 0;
+    
+    /* Weight force, which only affects the y-direction (because that's the direction gravity points). */
+    fy += m * 9.81;
+    
+    /* Air resistance force; this would affect both x- and y-directions, but we're only looking at the y-axis in this example. */
+    fy += -1 * 0.5 * rho * C_d * A * vy * vy;
+    
+    /* Verlet integration for the y-direction */
+    dy = vy * dt + (0.5 * ay * dt * dt);
+    /* The following line is because the math assumes meters but we're assuming 1 cm per pixel, so we need to scale the results */
+    y += dy * 100;
+    new_ay = fy / m;
+    avg_ay = 0.5 * (new_ay + ay);
+    vy += avg_ay * dt;
+    
+    /* Let's do very simple collision detection */
+    if (y + r > height && vy > 0)
+    {
+        /* This is a simplification of impulse-momentum collision response. e should be a negative number, which will change the velocity's direction. */
+        vy *= e; 
+        /* Move the ball back a little bit so it's not still "stuck" in the wall. */
+        y = height - r;                        
+    }
+    
     draw();
-};
+}
 
-var draw = function () {
-    ctx.strokeStyle = 'black';
+function draw()
+{
     ctx.clearRect(0, 0, width, height);
-    ctx.save();
-    ctx.translate(rect.topLeft.x, rect.topLeft.y);
-    ctx.rotate(rect.theta);
-    ctx.strokeRect(0, 0, rect.width, rect.height);
-    ctx.restore();
-
-    ctx.strokeStyle = '#cccccc';
     ctx.beginPath();
-    ctx.moveTo(spring.x, spring.y);
-    ctx.lineTo(rect.topLeft.x, rect.topLeft.y);
-    ctx.stroke();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.fill();
     ctx.closePath();
-};
-
+}
+   
+/* A real project should use requestAnimationFrame, and you should time the frame rate and pass a variable "dt" to your physics function. This is just a simple brute force method of getting it done. */
 setInterval(loop, dt * 1000);
